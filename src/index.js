a8a49239a2216a7bc00c284e2faa9e00a90924ed
Nana Pisanko <bonjozzy@gmail.com>
2023-01-23 00:13:26 +0300

    feat: resolve the task

--- src/index.js
+++ src/index.js
@@ -38,9 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    const space = '**********';
+    const mapOfChars = {
+        '10': '.',
+        '11': '-'
+    }
+    let wordsArray = expr.split(space);
+    let newArray = wordsArray.map(word => {
+        let index = 0;
+        let subArray = [];
+        for(let i = 0; i < word.length/10; i++){
+            let binaryLetter = word.slice(index, index + 10);
+            let binaryLetterLite;
+            for(let k = 0; k < binaryLetter.length; k++) {
+                if(binaryLetter[k] !== '0') {
+                    binaryLetterLite = binaryLetter.slice(k, binaryLetter.length);
+                    break;
+                }
+            }
+            let theWord = '';
+            for (let q = 0; q < binaryLetterLite.length; q+= 2) {
+                let chunkOfBinaryLetterLite = binaryLetterLite[q] + binaryLetterLite[q + 1];
+                theWord += mapOfChars[chunkOfBinaryLetterLite];
+            }
+            const result = MORSE_TABLE[theWord];
+            subArray.push(result);
+            index += 10;
+        }
+        return subArray.join('');
+    })
+    return newArray.join(' ');
 }
-
 module.exports = {
     decode
 }
